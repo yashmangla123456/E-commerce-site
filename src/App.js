0d7bdcc79639b29b254392c1f0a401fdec985b3d
Yash <yashmangla123456@gmail.com>
2024-01-02 18:17:06 +0530

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Shop />} />
+          <Route path="E-commerce-site/" element={<Shop />} />
           <Route path="/" element={<Shop />} />
           <Route
             path="/mens"
